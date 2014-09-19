63e25bfd2019e14f82a33b9b8fc298438cab67d6
Christoph Neumann <christoph@neumannhaus.com>
2014-09-19 12:20:17 -0700

    Illustrated some of the different parts: a component, setting up Om/React, launching the app.

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,15 +1,46 @@
 (ns app.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [sablono.core :as html :refer-macros [html]]))
 
+;; Sends "println" to the Javascript console
 (enable-console-print!)
 
-(def app-state (atom {:text "Hello World!"}))
+(def app-state (atom {:text "Hello Turkey"}))
 
-(om/root
-  (fn [app owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text app)))))
-  app-state
-  {:target (. js/document (getElementById "app"))})
+(defn- root-frame
+  [{:keys [text] :as app-state} owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:tag "green"})
+    om/IWillMount
+    (will-mount [_]
+      (println "will mount"))
+    om/IDidMount
+    (did-mount [_]
+      (println "did mount"))
+    om/IRenderState
+    (render-state [_ {:keys [tag] :as state}]
+      (html [:div {:ref "top"
+                   :className "TopView Box"
+                   :style {:background-color "#ccccff"}}
+              [:h1 text]
+              [:button tag]]))))
+
+(defn- root-frame-simple
+  [app owner]
+  (om/component
+    (html [:h1 (:text app)])))
+
+(defn- install-root!
+  [app-state]
+  (let [options {:target (. js/document (getElementById "app"))}]
+    (om/root root-frame app-state options)))
+
+(defn- main
+  []
+  (println "Welcome to the app")
+  (install-root! app-state))
+
+;; Set the window.onload property to point to the main function
+(set! (.-onload js/window) main)
